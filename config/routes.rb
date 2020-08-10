9881699fe5c7905f35047c200b134da4f297c8e3
vagrant <vagrant@localhost.localdomain>
2020-08-10 13:02:19 +0000

    [3-7]start

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 	get 'top' => 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     post 'todolists' => 'todolists#create'
+    get 'todolists' => 'todolists#index'
+    get 'todolists/:id' => 'todolists#show', as: 'todolist'
 end
